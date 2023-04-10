b3ed598f08cd86e4d01a06ba963283df96cc5552
Tomo4kap <tomo4kap@gmail.ru>
2023-04-10 18:51:13 +0300

    Mmax(3) - Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int Mmax = number1;
+if (number2 > Mmax)
+    Mmax = number2;
+
+if (number3 > Mmax)
+    Mmax = number3;
+Console.WriteLine($"Max = {Mmax}");
+
+
